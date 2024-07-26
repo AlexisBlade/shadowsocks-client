b07bdcc6348ae790c5b7ffd41dbd9d4ad635ef31
AlexisBlade <alexisbladetv@gmail.com>
2024-07-26 15:51:39 +0300

    feat: Add system proxy configuration for Windows
    - Added functions to set and unset system proxy using WinAPI
    - Integrated proxy configuration in the main function
    - Updated main.c to set system proxy to 127.0.0.1:1080 upon startup
    - Added cleanup to unset system proxy before exit
    - Ensured that proxy settings are applied and removed correctly
    
    This update allows the Shadowsocks client to automatically configure the system proxy settings for Windows, redirecting all network traffic through the local Shadowsocks proxy. The proxy settings are reverted back to their original state when the program exits.

--- src/main.c
+++ src/main.c
@@ -31,13 +31,13 @@ int inet_pton(int af, const char *src, void *dst) {
     strncpy(src_copy, src, INET6_ADDRSTRLEN+1);
     src_copy[INET6_ADDRSTRLEN] = 0;
 
-    if (WSAStringToAddress(src_copy, af, NULL, (struct sockaddr *)&ss, &size) == 0) {
+    if (WSAStringToAddressA(src_copy, af, NULL, (struct sockaddr *)&ss, &size) == 0) {
         switch (af) {
             case AF_INET:
                 *(struct in_addr *)dst = ((struct sockaddr_in *)&ss)->sin_addr;
                 return 1;
             case AF_INET6:
-                *(struct in6_addr *)dst = ((struct in6_addr *)&ss)->sin6_addr;
+                *(struct in6_addr *)dst = ((struct sockaddr_in6 *)&ss)->sin6_addr;
                 return 1;
         }
     }
